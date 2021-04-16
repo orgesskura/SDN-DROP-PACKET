a909ee4649c34ae6a301d75b20e5b3ba8fec56ab
orgesskura <s1813106@ed.ac.uk>
2021-04-16 22:08:32 +0100

    passing test but not on mininet

--- l4mirror.py
+++ l4mirror.py
@@ -45,6 +45,38 @@ class L4Mirror14(app_manager.RyuApp):
         out_port = 2 if in_port == 1 else 1
         #
         # write your code here
+        cond = len(tcph) == 0 or  len(iph)  == 0
+        if cond:
+            acts = [psr.OFPActionOutput(out_port)]
+        else :
+            ip_src = iph[0].src
+            ip_dst = iph[0].dst
+            sport = tcph[0].src_port
+            dport = tcph[0].dst_port
+            if in_port == 1:
+                acts = [psr.OFPActionOutput(2)]
+                mtc = psr.OFPMatch(in_port = 1,eth_type=eth.ethertype,ipv4_src=ip_src,ipv4_dst = ip_dst,ip_proto = iph[0].proto,tcp_src=sport,tcp_dst=dport)
+                self.add_flow(dp, 1, mtc, acts, msg.buffer_id)
+                if msg.buffer_id != ofp.OFP_NO_BUFFER:
+                    return
+            else :
+                match = (ip_src,ip_dst,sport,dport)
+                if tcph[0].has_flags(tcp.TCP_SYN) and not tcph[0].has_flags(tcp.TCP_ACK) :
+                    self.ht[match] = 1
+                    acts = [psr.OFPActionOutput(1)]
+                    acts.append(psr.OFPActionOutput(3))
+                elif match in self.ht:
+                    acts = [psr.OFPActionOutput(1)]
+                    self.ht[match] = self.ht[match] + 1
+                    acts.append(psr.OFPActionOutput(3))
+                else :
+                    return
+                if match in self.ht and self.ht[match] == 10 :
+                    self.ht.pop(match, None)
+                    mtc = psr.OFPMatch(in_port = 2,eth_type=eth.ethertype,ipv4_src=ip_src,ipv4_dst = ip_dst,ip_proto = iph[0].proto,tcp_src=sport,tcp_dst=dport)
+                    self.add_flow(dp, 1, mtc, acts, msg.buffer_id)
+                if msg.buffer_id != ofp.OFP_NO_BUFFER:
+                    return
         #
         data = msg.data if msg.buffer_id == ofp.OFP_NO_BUFFER else None
         out = psr.OFPPacketOut(datapath=dp, buffer_id=msg.buffer_id,
