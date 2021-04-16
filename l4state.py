8ccfccfd739311c10b9295920f5d8d46b647ee09
orgesskura <s1813106@ed.ac.uk>
2021-04-16 16:30:55 +0100

    task 1 done

--- l4state.py
+++ l4state.py
@@ -41,6 +41,47 @@ class L4State14(app_manager.RyuApp):
         eth = pkt.get_protocols(ethernet.ethernet)[0]
         #
         # write your code here
+        tcph = pkt.get_protocols(tcp.tcp)
+        iph = pkt.get_protocols(ipv4.ipv4)
+        dst, src = (eth.dst, eth.src)
+        cond = len(tcph) == 0 or  len(iph)  == 0
+        # way to drop a packet
+       # acts = [psr.OFPActionOutput(ofp.OFPPC_NO_FWD)]
+        if cond:
+            #do something
+            print('I got you')
+            if in_port == 1:
+                acts = [psr.OFPActionOutput(2)]
+            else :
+                acts = [psr.OFPActionOutput(1)]
+
+        else :
+            ip_src = iph[0].src
+            ip_dst = iph[0].dst
+            sport = tcph[0].src_port
+            dport = tcph[0].dst_port
+            match = (ip_src,ip_dst,sport,dport)
+            if in_port == 1:
+                #do somethinkg
+                if match not in self.ht:
+                    self.ht.add(match)
+                acts = [psr.OFPActionOutput(2)]
+                mtc = psr.OFPMatch(in_port=in_port,ip_proto = iph[0].proto,ipv4_src=ip_src,ipv4_dst = ip_dst,tcp_src=sport,tcp_dst=dport)
+                self.add_flow(dp, 1, mtc, acts, msg.buffer_id)
+                if msg.buffer_id != ofp.OFP_NO_BUFFER:
+                    return
+            else :
+                #do something
+                match2 = (ip_dst,ip_src,dport,sport)
+                if match2 in self.ht:
+                    acts = [psr.OFPActionOutput(1)]
+                    mtc = psr.OFPMatch(in_port=2,ip_proto = iph[0].proto,ipv4_src=ip_src,ipv4_dst = ip_dst,tcp_src=sport,tcp_dst=dport)
+                    self.add_flow(dp, 1, mtc, acts, msg.buffer_id)
+                    if msg.buffer_id != ofp.OFP_NO_BUFFER:
+                        return
+                else :
+                    acts = [psr.OFPActionOutput(ofp.OFPPC_NO_FWD)]
+
         #
         data = msg.data if msg.buffer_id == ofp.OFP_NO_BUFFER else None
         out = psr.OFPPacketOut(datapath=dp, buffer_id=msg.buffer_id,
